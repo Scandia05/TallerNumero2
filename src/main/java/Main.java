85b35e0db31dc60f49492d03f7d7bd68b599583d
hp <s.candia05@ufromail.cl>
2021-04-28 09:24:06 -0400

    Taller 2 Segundo commit 3 métodos de momento
    Signed-off-by: hp <s.candia05@ufromail.cl>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,24 @@
 import java.util.Scanner;
+import java.io.FileReader;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         ingresarPalabra();
+        leerTexto();
     }
-    static Scanner sc = new Scanner(System.in);
+
+    public static void leerTexto() {
+        try {
+            FileReader s = new FileReader("");
+
+        }catch (Exception e){
+
+        }
+
+    }
+
+
     public static String ingresarPalabra(){
         System.out.print("Introduce una frase: ");
         String s = sc.next() + sc.nextLine();
@@ -14,5 +28,25 @@ public class Main {
         s = s.replace(".", "");
         return s;
 
+
+    }
+    public static boolean validar(String s) {
+        int fin = s.length()-1;
+        int ini=0;
+        boolean b=true;
+
+        while(ini < fin){
+            if(s.charAt(ini)!=s.charAt(fin)){
+                b=false;
+            }
+            ini++;
+            fin--;
+        }
+        if(b)
+            System.out.print("\nEs palindromo.");
+        else
+            System.out.print("\nNo es palindromo.");
+
+        return b;
     }
 }
